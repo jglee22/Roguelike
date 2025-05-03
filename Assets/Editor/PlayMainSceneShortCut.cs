42b7f459ef55423c8e002ba28f3b015f4b78e36d
jglee <jglee@naver.com>
2025-05-03 18:47:05 +0900

    3차 커밋

---
+++ Assets/Editor/PlayMainSceneShortCut.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayMainSceneShortCut : MonoBehaviour
+{
+    [MenuItem("Tools/Play Main Scene %h")] // Ctrl + Shift + H
+    static void PlayMainScene()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            // 씬 저장 여부 확인
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                // 메인 씬 경로 (프로젝트 내 Main 씬 경로로 수정 필요!)
+                string mainScenePath = "Assets/Scenes/Main.unity";
+
+                EditorSceneManager.OpenScene(mainScenePath);
+                EditorApplication.isPlaying = true;
+            }
+        }
+    }
+}
---
+++ Assets/Editor/UpgradeCreator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class UpgradeCreator : MonoBehaviour
+{
+    [MenuItem("Tools/Create New Upgrade Option")]
+    public static void CreateUpgradeOption()
+    {
+        // 폴더 경로
+        string folderPath = "Assets/ScriptableObjects/Upgrades";
+
+        // 폴더 없으면 생성
+        if (!AssetDatabase.IsValidFolder(folderPath))
+            AssetDatabase.CreateFolder("Assets/ScriptableObjects", "Upgrades");
+
+        // 새 ScriptableObject 인스턴스 생성
+        UpgradeOption asset = ScriptableObject.CreateInstance<UpgradeOption>();
+
+        // 파일 이름 지정
+        string assetName = "NewUpgrade_" + System.Guid.NewGuid().ToString("N").Substring(0, 6);
+        string fullPath = $"{folderPath}/{assetName}.asset";
+
+        // 생성 및 저장
+        AssetDatabase.CreateAsset(asset, fullPath);
+        AssetDatabase.SaveAssets();
+
+        // 선택 상태로 만들기
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+
+        Debug.Log($"[UpgradeCreator] 생성됨: {fullPath}");
+    }
+}
