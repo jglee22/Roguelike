670a36a544a04366c460e5064e3fabba275f67bb
jglee <jglee@naver.com>
2025-05-16 11:03:36 +0900

    젠킨스 빌드용 스크립트 추가

---
+++ Assets/Editor/JenkinsAutoBuild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class JenkinsAutoBuild : MonoBehaviour
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+    출처: https://dev-junwoo.tistory.com/155 [개발초보 JW의 성장일기:티스토리]
+}
